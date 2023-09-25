14b59ba97d1801b948e5cc35b852679b16bd1ef3
Hasnaa <hassnaamohamed226@gmail.com>
2023-09-25 10:29:58 +0300

    Lap_4

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import AppRoutes from './AppRoutes';
+
+function App() {
+  return (
+    <div >
+      {/* routing module */}
+      
+     <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
+
