e7ec57e539cf379e692021d135f5dd830b8e91c2
Hassnaamohamed226 <94675965+Hassnaamohamed226@users.noreply.github.com>
2023-09-30 21:25:33 +0300

    AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
-
+import Favorites from './Pages/favorite'
 import NotFoundPage from "./Pages/NotFoundPage";
 
 import ProDetails from "./Pages/ProDetails";
 function AppRoutes() {
   return (
+    
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products/:id" element={<ProDetails />} />
         <Route path="*" element={<NotFoundPage />} />
+        <Route path="/favorites" component={<Favorites/>} />
       </Routes>
     </BrowserRouter>
   );
